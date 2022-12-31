fc24f8db2f3e22c027912c7cb80b5af7538b55b9
Shivamkumar <shivammessi07@gmail.com>
2022-12-31 10:10:56 +0530

    N/A

--- Dynamic Programming/diceCombinations.cpp
+++ Dynamic Programming/diceCombinations.cpp
@@ -1,34 +1,3 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// #define int long long
-// #define MOD 1000000007;
-
-// int solve(vector<int> &dp, int n){
-//     if(n==0){
-//         return 1;
-//     }
-//     if(n<0){
-//         return 0;
-//     }
-//     if(dp[n] != -1){
-//         return dp[n];
-//     }
-//     int ans=0;
-//     for(int i=1;i<=6;i++){
-//         ans += solve(dp, n-i);
-//         ans %= MOD;
-//     }
-//     return ans;
-// }
-
-// int32_t main(){
-//         int n;
-//         cin>>n;
-//         vector<int> dp(n+1, -1);
-//         cout<<solve(dp, n)<<endl;
-//     return 0;
-// }
-
 #include <bits/stdc++.h>
 using namespace std;
  
